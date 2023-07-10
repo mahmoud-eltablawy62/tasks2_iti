b641c6df3e0079988b39903d30199b2bfc2ce194
mahmoud-eltablawy62 <134718324+mahmoud-eltablawy62@users.noreply.github.com>
2023-07-10 13:50:18 +0300

    tasks_day2

---
+++ min_max/bubble sort/bubble sort/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace bubble_sort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[10];
+            for (int i = 0; i < arr.Length; i++) {
+                Console.WriteLine($"enter the num {i+1}");
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("==========================================================");
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j=0; j<9-i; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        int temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+                }
+            }
+            print(arr);
+        }
+        static void print(int [] arr  )
+        {
+            for (int i = 0;i < arr.Length;i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+        }
+    }
+}
