d23548bf43be44e97a78b484d98143d760118659
mahmoud-eltablawy62 <134718324+mahmoud-eltablawy62@users.noreply.github.com>
2023-07-10 14:03:59 +0300

    tasks_day2

---
+++ SearchInArray/SearchInArray/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace SearchInArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter the size of array");
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = new int[n];
+            for (int i=0; i<n; i++)
+            {
+                Console.WriteLine($"enter the num {i+1}");
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("enter the search number:-");
+            int search_number = int.Parse(Console.ReadLine());
+            int index = 0;
+            for (int i = 0; i < n; i++)
+            {
+               if(search_number == arr[i])
+                {
+                    index = i;
+                    break;
+                }
+            }
+            Console.WriteLine($"index of the search number {search_number} :-  {index} " );
+        }
+    }
+}
