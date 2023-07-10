56e9b95caf61583dcb985c33ed81b52d6c1d0d35
mahmoud-eltablawy62 <134718324+mahmoud-eltablawy62@users.noreply.github.com>
2023-07-10 13:15:51 +0300

    tasks_day2

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {    static void Main(string[] args)
+        {
+            int[] arr = new int[10];
+            int max = 0;
+            int min = 2000000000;
+            for (int i=0; i < arr.Length; i++)
+            {
+                Console.WriteLine($"enter the num {i+1} :- ");
+                arr[i] = int.Parse(Console.ReadLine());
+                if (arr[i] > max) max = arr[i];
+                if (arr[i] < min) min = arr[i];
+            }
+            Console.WriteLine($"max_number ==> {max} /n min_number==> {min} ");
+        }
+    }
+}
