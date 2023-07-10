6c34de3c50fafdd589a99669fd93fb4f61329575
mahmoud-eltablawy62 <134718324+mahmoud-eltablawy62@users.noreply.github.com>
2023-07-10 14:58:40 +0300

    tasks_day2

---
+++ check_unchecked/check_unchecked/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace check_unchecked
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int y = int.MaxValue;
+            int i = int.MaxValue;
+
+            checked
+            {
+                unchecked
+                {
+                    i++;
+                }
+            }
+            Console.WriteLine(i);
+            checked
+            {
+                y++;
+                unchecked
+                {
+                    i++;
+                }
+            }
+
+            
+            Console.WriteLine(y);
+            
+        }
+    }
+}
